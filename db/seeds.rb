02555b157783bfbbac3d93a527b295f495653586
imfaruk <habakana@gmail.com>
2022-09-27 07:42:03 +0600

    import seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,24 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  merchants = JSON.parse(File.read('./db/seeds/merchants.json'))
+  puts 'Creating merchants'
+  merchants['RECORDS'].each do |merchant|
+    Merchant.create!(merchant)
+  end
+
+  shoppers = JSON.parse(File.read('./db/seeds/shoppers.json'))
+  puts 'Creating shoppers'
+  shoppers['RECORDS'].each do |shopper|
+    Shopper.create!(shopper)
+  end
+
+  orders = JSON.parse(File.read('./db/seeds/orders.json'))
+  puts 'Creating orders'
+  orders['RECORDS'].each do |order|
+    Order.create!(order)
+  end
+
+  puts 'Creating disbursement fee rules'
+  DisbursementFeeRule.create(lower_bound: 0.0, upper_bound: 50, percentage: 1)
+  DisbursementFeeRule.create(lower_bound: 51.0, upper_bound: 300, percentage: 0.95)
+  DisbursementFeeRule.create(lower_bound: 301.0, upper_bound: Float::INFINITY, percentage: 0.85)
+end
